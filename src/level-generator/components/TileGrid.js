a6c202ce57b838364aed227747d2c2ebfcf3fa98
Dave Timmerman <dave@frontmen.nl>
2017-09-29 16:38:37 +0200

    + fied tiling

--- src/level-generator/components/TileGrid.js
+++ src/level-generator/components/TileGrid.js
@@ -8,13 +8,14 @@ class TileGrid extends React.PureComponent {
     const grid = this.props.grid;
     const width = grid[0].length;
     const height = grid.length;
+    const style = {height: `${this.props.tileSize}px`};
     const cursor = this.props.cursor || [undefined, undefined];
     for( let y=0; y < height; y++ ) {
       let row = [];
       for( let x=0; x < width; x++ ) {
         row.push(<Tile key={"tile" + x + "." + y} size={this.props.tileSize} tileNum={y*height+x} bright={x % 2 ^ y % 2} highlight={grid[y][x]+(cursor[0]==x&&cursor[1]==y)} />);
       }
-      tiles.push(<div key={"row" + y} className="tileRow">{row}</div>);
+      tiles.push(<div key={"row" + y} className="tileRow" style={style}>{row}</div>);
     }
     return <div className="tileGrid"><TileGridInfo gridInfo={this.props.gridInfo} />{tiles}</div>
   };
