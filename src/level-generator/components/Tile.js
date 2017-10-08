cc8b0a335bc985ec70a559dbe04b35cf88506a97
Han Bol <nash8114@gmail.com>
2017-10-08 19:19:27 +0200

    Fixing issues which arose from the merge with develop. Also added visual effect to indicate grid changes.

--- src/level-generator/components/Tile.js
+++ src/level-generator/components/Tile.js
@@ -3,17 +3,21 @@ import React from 'react';
 class Tile extends React.PureComponent {
   render() {
     const classes = ["tile"];
+    if( this.props.visible )
+      classes.push("visible");
     if ( this.props.bright )
       classes.push("bright");
     if ( this.props.highlight ) {
       if( this.props.highlight == 2 )
-        classes.push("cursor")
+        classes.push("cursor");
+      else if( this.props.highlight > 0 )
+        classes.push("highlight" + this.props.highlight);
       else
-        classes.push("highlight");
+        classes.push("strikeThrough" + this.props.highlight * -1);
     }
     const style = {
       width: this.props.size,
-       height: this.props.size,
+      height: this.props.size,
       display: "inline-block"
     };
     return <div className={classes.join(" ")} style={style}>{this.props.textContent}</div>
